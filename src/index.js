7272e1e2991886a388c20a4ff8440ed2f5f45845
distanceOn <distanceOn@yandex.ru>
2023-06-20 03:09:18 +0300

    Завершить создание локального сервера с требуемыми запросами, используя шаблон

--- src/index.js
+++ src/index.js
@@ -1,12 +1,44 @@
-const http = require('http');
+import http from "http";
+import fs from "fs";
+import path from "path";
+
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
 
 const server = http.createServer((request, response) => {
+  if (request.url === "/") {
+    response.statusCode = 200;
+    response.setHeader("Content-Type", "text/plain");
+    response.end("Hello, World!");
+  } else if (request.url.startsWith("/?hello=")) {
+    const name = request.url.split("=")[1];
 
-    // Написать обработчик запроса:
-    // - Ответом на запрос `?hello=<name>` должна быть **строка** "Hello, <name>.", код ответа 200
-    // - Если параметр `hello` указан, но не передано `<name>`, то ответ **строка** "Enter a name", код ответа 400
-    // - Ответом на запрос `?users` должен быть **JSON** с содержимым файла `data/users.json`, код ответа 200
-    // - Если никакие параметры не переданы, то ответ **строка** "Hello, World!", код ответа 200
-    // - Если переданы какие-либо другие параметры, то пустой ответ, код ответа 500
+    if (name) {
+      response.statusCode = 200;
+      response.setHeader("Content-Type", "text/plain");
+      response.end(`Hello, ${name}.`);
+    } else {
+      response.statusCode = 400;
+      response.setHeader("Content-Type", "text/plain");
+      response.end("Enter a name");
+    }
+  } else if (request.url === "/users") {
+    const filePath = path.join(__dirname, "data/users.json");
+    fs.readFile(filePath, (err, data) => {
+      if (err) {
+        response.statusCode = 500;
+        response.end();
+      } else {
+        response.statusCode = 200;
+        response.setHeader("Content-Type", "application/json");
+        response.end(data);
+      }
+    });
+  } else {
+    response.statusCode = 500;
+    response.end();
+  }
+});
 
+server.listen(3003, () => {
+  console.log(`Сервер запущен по адресу http://127.0.0.1:3003`);
 });
